de2cca031ded6411a4e6f2e8327f9a34ae5e6385
Chris Waddington <cwadding@gmail.com>
2015-09-03 09:43:20 -0400

    use Promise.reject rather than throwing the error

--- src/common/response-handlers.js
+++ src/common/response-handlers.js
@@ -2,11 +2,12 @@ var _checkStatus = function(response) {
   if (response.status >= 200 && response.status < 300) {
     return response;
   } else {
+
     var error = new Error(response.statusText);
-    var buffer = new Buffer(response.body._readableState.buffer[0]);
-    error.response = response;
-    error.response.body = JSON.parse(buffer.toString('utf-8'));
-    throw error;
+    return response.json().then(function(msg) {
+      error.body = msg;
+      return Promise.reject(error);
+    });
   }
 };
 
